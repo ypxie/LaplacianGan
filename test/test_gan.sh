66f6023752b33faab52b5b6ed6a80137c979a48d
Yuanpu Xie <shampool@ufl.edu>
2017-10-09 13:34:05 -0400

    testing gan

--- test/test_gan.sh
+++ test/test_gan.sh
@@ -1,10 +1,16 @@
 
-name='zz_mmgan_plain_gl_disc'
+#name='zz_mmgan_plain_gl_disc'
+#gen='origin'
+#disc='origin'
+#python test_gan.py --device_id 3 --batch_size 32  --imsize 256 --model_name ${name}  --load_from_epoch 500 --which_gen ${gen} --which_disc ${disc} #
+
+#name='zz_mmgan_plain_gl_disc_baldg'
+#python test_gan.py --device_id 3 --batch_size 32  --imsize 256 --model_name ${name}  --load_from_epoch 500 --which_gen ${gen} --which_disc ${disc} 
+
+name='large_plan'
 gen='origin'
-disc='origin'
+disc='origin_global_local'
+python test_gan.py --device_id 3 --batch_size 32  --imsize 256 --model_name ${name}  --load_from_epoch 237 --which_gen ${gen} --which_disc ${disc} #
 
-python test_gan.py --device_id 3 --batch_size 16  --imsize 256 --model_name ${name}  --load_from_epoch 500 --which_gen ${gen} --which_disc ${disc} 
-
-name='zz_mmgan_plain_gl_disc_baldg'
-python test_gan.py --device_id 3 --batch_size 16  --imsize 256 --model_name ${name}  --load_from_epoch 500 --which_gen ${gen} --which_disc ${disc} 
-
+rm -r /data/data2/yuanpu/Results
+cp -r ../Data/Results  /data/data2/yuanpu/
